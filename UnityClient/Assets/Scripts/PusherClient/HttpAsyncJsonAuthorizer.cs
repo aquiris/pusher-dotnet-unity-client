507c3543ea55f8378a48ea8b7537f4af8f756c3f
Danonne <rafael.pacheco@aquiris.com.br>
2017-08-01 14:25:04 -0300

    Fix certificate validation on https

--- UnityClient/Assets/Scripts/PusherClient/HttpAsyncJsonAuthorizer.cs
+++ UnityClient/Assets/Scripts/PusherClient/HttpAsyncJsonAuthorizer.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Net;
+using System.Net.Security;
+using System.Security.Cryptography.X509Certificates;
 
 namespace PusherClient
 {
@@ -20,6 +22,13 @@ namespace PusherClient
         {
             _authEndpoint = new Uri(authEndpoint);
             _responseMap = new Dictionary<WebClient, AuthResponse>();
+
+            ServicePointManager.ServerCertificateValidationCallback += CustomServerCertificateValidationCallback;
+        }
+
+        ~HttpAsyncJsonAuthorizer()
+        {
+            ServicePointManager.ServerCertificateValidationCallback -= CustomServerCertificateValidationCallback;
         }
 
         public void Authorize(string channelName, string socketId, Action<string, string> onAuthResponse)
@@ -53,6 +62,37 @@ namespace PusherClient
             {
                 authResponseObj.OnAuthResponse(authResponseObj.ChannelName, eventArgs.Result);
             }
+        }
+
+        private static bool CustomServerCertificateValidationCallback(object sender,
+                                                                      X509Certificate certificate, X509Chain chain,
+                                                                      SslPolicyErrors sslPolicyErrors)
+        {
+            bool isOk = true;
+
+            // If there are errors in the certificate chain,
+            // look at each error to determine the cause.
+            if(sslPolicyErrors != SslPolicyErrors.None)
+            {
+                for(int i = 0; i < chain.ChainStatus.Length; i++)
+                {
+                    if(chain.ChainStatus[i].Status == X509ChainStatusFlags.RevocationStatusUnknown)
+                    {
+                        continue;
+                    }
+                    chain.ChainPolicy.RevocationFlag = X509RevocationFlag.EntireChain;
+                    chain.ChainPolicy.RevocationMode = X509RevocationMode.Online;
+                    chain.ChainPolicy.UrlRetrievalTimeout = new TimeSpan(0, 1, 0);
+                    chain.ChainPolicy.VerificationFlags = X509VerificationFlags.AllFlags;
+                    bool chainIsValid = chain.Build((X509Certificate2) certificate);
+                    if(!chainIsValid)
+                    {
+                        isOk = false;
+                        break;
+                    }
+                }
+            }
+            return isOk;
         }
     }
 }
